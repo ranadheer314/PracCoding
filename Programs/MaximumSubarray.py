9785fa521301c31a4b76c2769949100eca96e8e3
Ranadheer <ranadheer314@gmail.com>
2020-07-12 21:35:19 -0400

    Create MaximumSubarray.py

---
+++ Programs/MaximumSubarray.py
@@ -0,0 +1,10 @@
+def maxSubArray(nums) -> 'int':
+    n = len(nums)
+    max_sum = nums[0]
+    for i in range(1, n):
+        if nums[i - 1] > 0:
+            nums[i] += nums[i - 1]
+        max_sum = max(nums[i], max_sum)
+    return max_sum
+nums=[1,-1,5,5]
+print(maxSubArray(nums))
